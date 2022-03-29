5604766bf35d78537ed76ea6398826370999eb03
LazyTurtle4795 <102580403+LazyTurtle4795@users.noreply.github.com>
2022-03-29 12:57:21 +0530

    Add files via upload

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+var i = 0;
+var txt = 'I make memes and software';
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter()
